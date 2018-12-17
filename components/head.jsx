96b878bbfaac93924bd5e634bfed671642b57f4c
Jhonatan da Silva Batista <talkto@jhonatansilva.com>
2018-12-16 22:48:30 -0200

    feat: add meta viewport

--- components/head.jsx
+++ components/head.jsx
@@ -28,6 +28,7 @@ const Head = props => (
       name="description"
       content={props.description || defaultDescription}
     />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
   </NextHead>
 )
 
