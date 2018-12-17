80c64dd7dc210aa7a6e8d7a9dc555625dbefc114
Jhonatan da Silva Batista <talkto@jhonatansilva.com>
2018-12-16 22:15:12 -0200

    refactor: googletag

--- components/head.jsx
+++ components/head.jsx
@@ -8,15 +8,20 @@ const defaultOGImage = ''
 
 const Head = props => (
   <NextHead>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131081465-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-131081465-1');
-    </script>
-
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','UA-131081465-1');`
+      }}
+    />
+    <noscript
+      dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=UA-131081465-1" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`
+      }}
+    />
     <meta charSet="UTF-8" />
     <title>{props.title || ''}</title>
     <meta
