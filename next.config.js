8edc2f92c10730c51a14886739c12ec0d00d5659
jhonnyskywalker <talkto@jhonatansilva.com>
2020-06-23 06:47:45 -0300

    fix: 🐛 add export path map config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  /* config options here */
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/contact': { page: '/contact' },
+      '/lab': { page: '/lab' },
+      '/experience': { page: '/experience' },
+    }
+  },
 }
